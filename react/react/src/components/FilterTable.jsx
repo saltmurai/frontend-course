2951deb692cf4e2f73c44368abbcad9b02c37e5d
saltmurai <phamsang6260@gmail.com>
2024-01-12 21:26:35 +0700

    feat: update

--- react/react/src/components/FilterTable.jsx
+++ react/react/src/components/FilterTable.jsx
@@ -1,14 +1,34 @@
+import { useState } from "react";
+
 /* eslint-disable react/prop-types */
-function SearchBar() {
+function SearchBar({fruitList, setFilter}) {
+	const [searchTerm, setSearchTerm] = useState("");
+
+	function handleSearch(e){
+		setSearchTerm(e.target.value)
+		
+		if(e.target.value === "") {
+			setFilter(fruitList)
+			return
+		}
+
+		const filterItems = fruitList.filter((item) => {
+			return item.name.startsWith(e.target.value)
+		})
+		setFilter(filterItems)
+	}
+
+	
   return (
     <div className="flex flex-col justify-start">
-      <input />
+      <input value={searchTerm} onChange={handleSearch} />
       <div>
         <input type="checkbox" /> Only in stock
       </div>
     </div>
   );
 }
+
 function Product({item}){
 return (
 <div className="bg-blue-500">
@@ -21,30 +41,47 @@ return (
 }
 
 function Table({fruitList}) {
+	const fruitRows = []
+	const vegRows = []
+
+	fruitList.forEach((item) => {
+		if(item.category === "Fruits") {
+			fruitRows.push(<Product item={item} />)
+		}
+		if(item.category === "Vegetables") {
+			vegRows.push(<Product item={item} />)
+		}
+	})
+
   return (
 		<div className="flex flex-col gap-4">
-		<Product item={fruitList[0]} />
-		<Product item={fruitList[1]} />
-		<Product item={fruitList[2]} />
-		<Product item={fruitList[4]} />
+			<div>
+				<h4>Fruits</h4>
+				{fruitRows}
+			</div>
+			<div>
+				<h4>Vegetables</h4>
+				{vegRows}
+			</div>
 		</div>
 
 	);
 }
 
 function FilterTable() {
-  const fruits = [
+	const fruits = [
     { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
     { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
     { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
     { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
     { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
     { category: "Vegetables", price: "$1", stocked: true, name: "Peas" },
-  ];
+  ]
+	const [filter, setFilter] = useState(fruits)
   return (
     <div>
-      <SearchBar />
-      <Table fruitList={fruits} />
+      <SearchBar fruitList={fruits} setFilter={setFilter} />
+      <Table fruitList={filter} />
     </div>
   );
 }
